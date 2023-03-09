6015ceb16c99d1e1f73d05ea04c65d2087b99594
Mehran Ghamaty <mehran@ghamaty.me>
2023-03-09 12:02:37 -0800

    added a different chrome extension

---
+++ chromeExtensionAutoDetector/background.js
@@ -0,0 +1,8 @@
+// Regex-pattern to check URLs against. 
+// It matches URLs like: http[s]://[...]stackoverflow.com[...]
+var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?stackoverflow\.com/;
+
+// A function to use as callback
+function doStuffWithDom(domContent) {
+    console.log('I received the following DOM content:\n' + domContent);
+}
